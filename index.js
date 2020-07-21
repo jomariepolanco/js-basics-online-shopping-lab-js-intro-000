89be4b6e2ce33e6d75d9f952defd72c3c874dd5d
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-07-21 21:53:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ function viewCart() {
   // write your code here
   if (cart.length === 0) {
     return "Your shopping cart is empty."
+  } else {
+    cartItems = []
+    var last = `${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
+    for (let i = 0; i < cart.length - 1; i++) {
+      cartItems.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+    }
+    return `In your cart, you have ${cartItems.join(', ')}, and ${last}`
   }
 }
 
