37f9560f2a00a15132857bacd4104be009e72e7a
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-07-21 21:59:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ function viewCart() {
   if (cart.length === 0) {
     return "Your shopping cart is empty."
   } else if (cart.length === 1) {
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+    car oneItem =  `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+    return oneItem;
   } else if (cart.length === 2) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}. `
   }
