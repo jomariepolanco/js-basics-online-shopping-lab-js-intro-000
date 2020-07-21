cd9be5f3a13fe5a3379526a2a17f233ccfda90b5
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-07-21 21:33:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -77,5 +77,10 @@ for (let i = 0; i < cart.length; i++) {
 
 function placeOrder(cardNumber) {
   // write your code here
-
+if (typeof(cardNumber) === "undefined"){
+  return "Sorry, we don't have a credit card on file for you."
+} else {
+  cart = [];
+  return `Your total cost is $${total}, which will be charged to the cart ${cardNumber}.`
+}
 }
